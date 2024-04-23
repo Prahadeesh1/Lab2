8aeb96c5f3c1f3c1e5c7c1beee1966d31d787ca1
Prahadeesh <Prahadeesh.23@ichat.sp.edu.sg>
2024-04-23 13:04:06 +0800

    Lab2

--- Lab2.py
+++ Lab2.py
@@ -1,8 +1,34 @@
 def calculate_bmi(height, weight):
-    print("Height = " + height)
-    print("Weight = " + weight)
-    calculate_bmi(weight="57", height="1.73")
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = int(weight/(height*height))
+    if (bmi<18.5):
+        print(bmi)
+        print("Underweight")
+    elif (18.5<bmi>25.0,bmi == 18.5, bmi ==25.0):
+        print(bmi)
+        print("Normal weight")
+    else:
+        print(bmi)
+        print("Overweight")
     
+
+    
+
+   
+
+
+
+calculate_bmi(weight=57, height=1.73)
+
+
+
+
+
+
+    
+
 
 
 
