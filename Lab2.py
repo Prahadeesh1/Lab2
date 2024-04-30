b2df99ace9a10b1fc6b1e6478612ab7d537eef99
Prahadeesh <Prahadeesh.23@ichat.sp.edu.sg>
2024-04-30 13:38:29 +0800

    Change

--- Lab2.py
+++ Lab2.py
@@ -1,20 +1,25 @@
 
-#def calculate_bmi(height, weight):
- #   print("Height = " + str(height))
-  #
-   # bmi = int(weight/(height*height))
-    #if (bmi<18.5):
-#        print(bmi)
- #       print("Underweight")
-#    elif (18.5<bmi>25.0,bmi == 18.5, bmi ==25.0):
-#        print(bmi)
-#        print("Normal weight")
-#    else:
-#        print(bmi)
-#        print("Overweight")
-#calculate_bmi(weight=57, height=1.73)
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+  
+    bmi = int(weight/(height*height))
+
+   
+    if (bmi<18.5):
+        print(bmi)
+       print("Underweight")
+    elif (18.5<bmi>25.0,bmi == 18.5, bmi ==25.0):
+        print(bmi)
+        print("Normal weight")
+    else:
+        print(bmi)
+        print("Overweight")
+calculate_bmi(weight=57, height=1.73)
 
 
+
+
+"""
 import statistics
 
 
@@ -68,7 +73,7 @@ if __name__ == "__main__":
     main()
 
 
-
+"""
 
 
     
